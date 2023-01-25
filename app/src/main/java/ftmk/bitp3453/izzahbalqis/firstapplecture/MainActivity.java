ab47bd1c934bde73af5f6b1de7219c268b9979f7
izzah <izzahbalqis112@gmail.com>
2023-01-25 08:54:34 +0800

    final commit

---
+++ app/src/main/java/ftmk/bitp3453/izzahbalqis/firstapplecture/MainActivity.java
@@ -0,0 +1,16 @@
+package ftmk.bitp3453.izzahbalqis.firstapplecture;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView textView = findViewById(R.id.txtvwWorks);
+    }
+}
